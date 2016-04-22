cd8004abe604aecf5bc9f5903b76ea9c7b07db18
Jason Shin <jasonshin98@gmail.com>
2016-04-22 00:04:04 -0400

    animation started, collision and removing previous balls needs to be fixed

--- animate.js
+++ animate.js
@@ -2,6 +2,14 @@
 var SVGNS = "http://www.w3.org/2000/svg",
     XLINKNS = "http://www.w3.org/1999/xlink";
 
+// Buttons
+var makeButton = document.getElementById("makeBall");
+var stopButton = document.getElementById("stop");
+var startButton = document.getElementById("start");
+var clearButton = document.getElementById("clear");
+
+var intervalID;
+
 var ballz = []
 
 var makeBall = function() {
@@ -13,51 +21,89 @@ var makeBall = function() {
         dx : Math.floor(Math.random() * (3 - (-3) + 1)) -3,
         dy : Math.floor(Math.random() * (3 - (-3) + 1)) -3,
         //random color
-        color : '#'+(Math.random()*0xFFFFFF<<0).toString(16) 
-        //collision method?
+        color : '#'+(Math.random()*0xFFFFFF<<0).toString(16),
+	//random size
+	r : Math.floor(Math.random() * (20)) + 2,
+        //calculates new vel. Needs adjustment so that if x or y 
+	//goes past 500 by too much, 
+	calcVel : function() {
+	    if (this.x == 0 || this.x == 500 - this.r) {
+		this.dx *= -1;
+            } if (this.y == 0 || this.y == 500 - this.r) {
+		this.dy *= -1;
+            }
+	    
+	}
     }
 };
 
-var initBall = function() {
+//Initializes ball
+var initBall = function(e) {
     var b = makeBall();
     ballz.push(b);
     drawBall(b);
     printBallz();
 };
 
+//Prints ballz to console
 var printBallz = function() {
     for (var i = 0; i < ballz.length; i++) {
         console.log(ballz[i]);
     }
 };
 
+//Ball Animation
+var animateBallz = function(e) {
+    window.clearInterval(intervalID);
+    //Erasing old balls doesn't seem to work
+    while (pic.lastChild) {
+	pic.removeChild(pic.lastChild);
+    };
+    //Function that moves ball
+    var animateCode = function() {
+	for (var i = 0; i < ballz.length; i++) {
+	    var ball = ballz[i];
+	    ball.calcVel();
+	    ball.x += ball.dx
+	    ball.y += ball.dy
+	    drawBall(ball);
+	}
+    };
+    //Animattion
+    intervalID = window.setInterval(animateCode, 16);
+};
+
+//Function to draw ball to svg
 var drawBall = function(b) {
-    var c = document.createElementNS(SVGNS,"ball");
+    var c = document.createElementNS(SVGNS,"circle");
     c.setAttribute("cx",b.x);
     c.setAttribute("cy",b.y);
-    c.setAttribute("r",2);
+    c.setAttribute("r",b.r);
     c.setAttribute("fill",b.color);
     c.setAttribute("stroke","black");
     pic.appendChild(c);
 }
 
-var pic = document.getElementById("vimage"),
-    ballbtn = document.getElementById("ball"),
-    stopbtn = document.getElementById("stop");
+var pic = document.getElementById("vimage")
 
-var stopper = function() {
-    clearAll();
-    window.clearInterval(intervals[iCount - 1]);
-    iCount--;
+//Stops svg
+var stopper = function(e) {
+    //clearAll();
+    window.clearInterval(intervalID);
+    //iCount--;
 }
 
-var clearAll = function() {
+var clearAll = function(e) {
     var children = pic.children;
     for (var i = children.length - 1; i >= 0; i--) {
         pic.removeChild(children[i]);
     }
 };
 
+makeButton.addEventListener("click", initBall);
+startButton.addEventListener("click", animateBallz);
+stopButton.addEventListener("click", stopper);
+clearButton.addEventListener("click", clearAll);
 //Initializes DVD animation
 var logo_init = function() {
     var c = document.createElementNS(SVGNS,"image");
